5c3e2f775f2e30584e5e12557b5b837fafa80812
Brian <wilsonedit@gmail.com>
2019-12-12 14:35:36 -0600

    Adding and deleting gear is working! UI tweaks needed

---
+++ YourPhotoKit/Models/TripGearViews/AddGearToTripViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YourPhotoKit.Models.TripGearViews
+{
+    public class AddGearToTripViewModel
+    {
+        public Trip Trip { get; set; }
+        public int TripId { get; set; }
+        public int GearItemId { get; set; }
+        public GearItem GearItem { get; set; }
+        public TripGear TripGear { get; set; }
+        public virtual ICollection<GearItem> GearItems { get; set; }
+        public virtual ICollection<TripGear> PickedItems { get; set; }
+
+
+    }
+}
