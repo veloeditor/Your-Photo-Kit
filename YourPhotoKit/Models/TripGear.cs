77c64e5214696ad10f0c3167e761e24e3fb7a0b5
Brian <wilsonedit@gmail.com>
2020-01-08 11:38:51 -0600

    tweaked basic styles for login, register

--- YourPhotoKit/Models/TripGear.cs
+++ YourPhotoKit/Models/TripGear.cs
@@ -20,7 +20,7 @@ namespace YourPhotoKit.Models
 
         public bool IsPacked { get; set; }
 
-        public Trip Trip { get; set; }
+        public Trip Trip { get; set; } = new Trip();
 
         [Display(Name = "Packed Gear")]
         public GearItem GearItem { get; set; }
